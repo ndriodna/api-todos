12521ee4e3816c19f6f4d8bbaedbc1b8f29cbb70
ndriodna <kurodesu6969@gmail.com>
2024-11-13 22:47:17 +0800

    add: error exception

---
+++ src/exception/error.js
@@ -0,0 +1,10 @@
+export function NotFoundError(msg) {
+    return { code: 404, status: 'Not Found', message: 'DATA NOT FOUND' }
+}
+export function InternalServerError(msg) {
+    return { code: 500, status: 'Internal Server Error', message: msg }
+}
+
+export function UnauthorizedError(msg) {
+    return { code: 401, status: 'Unauthorized', message: msg }
+}
