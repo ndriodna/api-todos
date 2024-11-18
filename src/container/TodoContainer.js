df139c40d495ca659eb9dd485a0958a50393696c
ndriodna <kurodesu6969@gmail.com>
2024-11-18 23:35:17 +0800

    fix: container depency injection

--- src/container/TodoContainer.js
+++ src/container/TodoContainer.js
@@ -3,7 +3,9 @@ import todoService from "../service/TodoService.js";
 import todoController from "../controller/TodoController.js";
 import { db } from "../db/db.js"
 
-const container = {
-    TodoController: todoController(todoService(todoRepository(db)))
-}
-export default container
+
+const TodoRepository = todoRepository(db)
+const TodoService = todoService(TodoRepository)
+const TodoController = todoController(TodoService)
+
+export { TodoController }
