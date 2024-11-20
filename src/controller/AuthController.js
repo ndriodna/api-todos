80f10942be72828b55f75c015bdd71f4677cf246
ndriodna <kurodesu6969@gmail.com>
2024-11-20 22:42:28 +0800

    add: auth controller

---
+++ src/controller/AuthController.js
@@ -0,0 +1,31 @@
+const AuthController = (AuthService) => ({
+    Auth: async (req, res, next) => {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    },
+    Login: async (req, res, next) => {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    },
+    Register: async (req, res, next) => {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    },
+    ForgotPassword: async (req, res, next) => {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    },
+})
+export default AuthController
