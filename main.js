0f48610dc8614c9c55825e726a64809ef8c57a9a
ndriodna <kurodesu6969@gmail.com>
2024-11-25 22:14:27 +0800

    add: cors

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import router from './src/routes/index.js';
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT
-
+app.use(cors())
 app.use(express.json())
 app.use('/', router)
 app.listen(port, process.env.SERVER, () => {
