d358b5b9c40c06dbcd3fb1b0d8ff793a96f2de94
ndriodna <kurodesu6969@gmail.com>
2024-12-25 00:05:59 +0800

    fix: otp max 6 length without zero val

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -1,7 +1,7 @@
 export default function generateOTP() {
     let otp = ''
     for (let i = 0; i < 6; i++) {
-        otp += Math.floor(Math.random() * 10)
+        otp += Math.floor(Math.random() * 9) + 1
     }
     return otp
 }
