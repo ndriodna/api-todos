46ca81feefeb666bc9bd938fd171106dccee45e2
ndriodna <kurodesu6969@gmail.com>
2024-12-27 23:19:16 +0800

    fix: checkpassword

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,20 +1,26 @@
-import bycrypt from 'bcrypt'
+import bcrypt from 'bcrypt'
 import { InternalServerError, UnauthorizedError } from '../exception/error.js'
 
-const saltOrRounds = 10
+const saltRounds = 10
 
 export const GeneratePassword = (password) => {
     return new Promise((resolve, reject) => {
-        bycrypt.hash(password, saltOrRounds, (err, encrypted) => {
-            if (err) reject(InternalServerError(err))
+        bcrypt.hash(password, saltRounds, (err, encrypted) => {
+            if (err) {
+                reject(InternalServerError(err))
+            }
             resolve(encrypted)
         })
     })
 }
 export const CheckPassword = (password, encrypted) => {
     return new Promise((resolve, reject) => {
-        bycrypt.compare(password, encrypted, (err, result) => {
-            if (err) reject(UnauthorizedError(err))
+        bcrypt.compare(password, encrypted, (err, result) => {
+            if (err) {
+                reject(UnauthorizedError(err))
+            } else if (!result) {
+                reject(UnauthorizedError('password invalid'))
+            }
             resolve(result)
         })
     })
