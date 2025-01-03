36091c636fa52264b46a01b088db3d2c35cc17a0
ndriodna <kurodesu6969@gmail.com>
2025-01-03 23:33:05 +0800

    add: query log

--- src/db/db.js
+++ src/db/db.js
@@ -8,6 +8,14 @@ export const Pool = new pg.Pool({
     max: 10
 })
 
+// override the query method to log the sql query
+Pool.on('connect', (client) => {
+    const originalQuery = client.query;
+    client.query = (...args) => {
+        console.log('query log: ', args[0]);
+        return originalQuery.apply(client, args);
+    };
+});
 export const transaction = (client) => ({
     BEGIN: () => {
         return client.query('BEGIN')
